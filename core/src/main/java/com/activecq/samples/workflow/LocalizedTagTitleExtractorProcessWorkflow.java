32e235d69458d24c8c5f3e2524ec8128e4b5c874
david <github@activecq.com>
2013-01-29 19:00:55 -0500

    Added comments for sample

--- core/src/main/java/com/activecq/samples/workflow/LocalizedTagTitleExtractorProcessWorkflow.java
+++ core/src/main/java/com/activecq/samples/workflow/LocalizedTagTitleExtractorProcessWorkflow.java
@@ -88,12 +88,10 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
     public static final String PROPERTY_TAG_TITLES = "tag-titles";
     public static final String PROPERTY_CQ_TAGS = "cq:tags";
 
-
-
     /** OSGi Service References **/
 
     @Reference
-    ResourceResolverFactory resourceResolverFactory;
+    private ResourceResolverFactory resourceResolverFactory;
 
     /** Fields **/
 
@@ -113,42 +111,61 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
         // Get the path to the JCR resource from the payload
         String path = workflowData.getPayload().toString();
 
-        // Get RR for the Workflow's executing session
+        // Get a ResourceResolver using the same permission set as the Workflow's executing Session
+        ResourceResolver resolver = null;
         Map<String, Object> authInfo = new HashMap<String, Object>();
         authInfo.put(JcrResourceConstants.AUTHENTICATION_INFO_SESSION, session);
 
+        // Initialize some variables
         List<String> newTagTitles = new ArrayList<String>();
         Locale locale = null;
 
         try {
+            // Get the Workflow Sessions' resource resolver using the authInfo created above
+            resolver = resourceResolverFactory.getResourceResolver(authInfo);
 
-            final ResourceResolver resolver = resourceResolverFactory.getResourceResolver(authInfo);
+            // Get the Resource representing the WF payload
             final Resource resource = resolver.getResource(path);
+
+            // Get the TagManager (using the same permission level as the Workflow's Session)
             final TagManager tagManager = resolver.adaptTo(TagManager.class);
+
+            // Use custom implementation to find the resource to look for cq:tags and write the
+            // custom property "tag-titles" to
             final Resource contentResource = getContentResource(resource);
 
             if(contentResource == null) {
-                log.error("Could not find a valid content resource node from payload: {}", resource.getPath());
+                log.error("Could not find a valid content resource node for payload: {}", resource.getPath());
                 return;
             }
 
+            // Gain access to the content resournce's properties
             final ValueMap properties = contentResource.adaptTo(ValueMap.class);
 
+
+            // Get the full tag paths (namespace:path/to/tag) from the content resource
+            // This only works on the cq:tags property
             final String[] tags = properties.get(PROPERTY_CQ_TAGS, new String[]{});
+
+            // Get any previously applied Localized Tag Titles.
+            // This is used to determine if changes if any updates are needed to this node.
             final String[] previousTagTitles = properties.get(PROPERTY_TAG_TITLES, new String[]{});
 
             if(!ArrayUtils.isEmpty(tags)) {
                 // Derive the locale
                 if(DamUtil.isAsset(resource)) {
+                    // Dam assets use path segments to derive the locale (/content/dam/us/en/...)
                     locale = getLocaleFromPath(resource);
                 } else {
+                    // Page's use the jcr:language property accessed via the CQ Page API
                     Page page = resource.adaptTo(Page.class);
                     if(page != null) {
                         locale = page.getLanguage(true);
                     }
                 }
 
-                // Get the New Localized Tag Titles
+                // Derive the Localized Tag Titles for all tags in the tag hierarchy from the Tags stored in the cq:tags property
+                // This does not remove duplicate titles (different tag trees could repeat titles)
                 newTagTitles = tagPathsToLocalizedTagTitles(tags, locale, tagManager);
             }
 
@@ -158,6 +175,8 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
 
                 // If the currently applied Tag Titles are the same as the derived Tag titles then skip!
                 if(!isSame(newTagTitles.toArray(new String[]{}), previousTagTitles)) {
+                    // If changes have been made to the Tag Names, then apply to the tag-titles property
+                    // on the content resource.
                     node.setProperty(PROPERTY_TAG_TITLES, newTagTitles.toArray(new String[newTagTitles.size()]));
                 } else {
                     log.debug("No change in Tag Titles. Do not update this content resource.");
@@ -169,7 +188,13 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
                 log.error(ex.getMessage());
             }
         } catch(LoginException ex) {
-          log.error(ex.getMessage());
+            log.error(ex.getMessage());
+        } finally {
+            // Clean up after yourself please!!!
+            if(resolver != null) {
+                resolver.close();
+                resolver = null;
+            }
         }
     }
 
@@ -208,13 +233,12 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
      * @return
      */
     private List<String> tagPathsToLocalizedTagTitles(String[] tagPaths, Locale locale, TagManager tagManager) {
-        int maxLoops = 1000;
         List<String> terms = new ArrayList<String>();
 
         for(String tagPath : tagPaths){
             boolean isLast = false;
 
-            int count = StringUtils.countMatches(tagPath, "/");
+            int count = StringUtils.countMatches(tagPath, PATH_DELIMITER);
             while(count >= MIN_TAG_DEPTH && count >= 0) {
 
                 Tag tag = tagManager.resolve(tagPath);
@@ -311,7 +335,9 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
             if(parent != null &&
                     isPrimaryType(parent, TYPE_CQ_PAGE) &&
                     StringUtils.equals(payloadResource.getName(), JcrConstants.JCR_CONTENT)) {
-                // cq:Page's jcr:content
+
+                /** cq:Page / jcr:content(nt:unstructured) **/
+
                 return payloadResource;
             } else if(StringUtils.equals(payloadResource.getName(), TYPE_DAM_ASSET_METADATA)) {
                 if(parent != null && StringUtils.equals(parent.getName(), JcrConstants.JCR_CONTENT)) {
@@ -321,7 +347,7 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
                     }
 
                     if(grandParent != null && isPrimaryType(grandParent, TYPE_DAM_ASSET)) {
-                        // dam:Asset metadata node
+                        /** dam:Asset / jcr:content / metadata **/
                         return payloadResource;
                     }
                 }
@@ -332,6 +358,8 @@ public class LocalizedTagTitleExtractorProcessWorkflow implements WorkflowProces
 
             return payloadResource.getChild(REL_PATH_DAM_ASSET_METADATA);
         }
+
+        /** Use the payload resource; Ex. a component resource that uses cq:tags **/
 
         return payloadResource;
     }
